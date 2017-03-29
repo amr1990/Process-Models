397e5b193828835b7cd653731f0e10049ff8caa0
Albert Mari <amr1990lol@gmail.com>
2017-03-29 09:20:17 +0200

    Some models implemented and changed views

--- gw2_webclient/gw2/models.py
+++ gw2_webclient/gw2/models.py
@@ -1,51 +1,41 @@
 from __future__ import unicode_literals
 
-#from django.db import models
-import requests
-import json
+from django.db import models
+
 
 # Create your models here.
 
-class Profession(object):
-    profession = ""
-    URL = "https://api.guildwars2.com/v2/professions/"
+class Profession(models.Model):
+    name = models.TextField(primary_key=True)
+    skillid = models.ForeignKey(ProfessionSkill)
+    especializations = models.TextField()
+    weapontypesid = models.ForeignKey(Weapon)
 
-    def __init__(self, professionname):
-        self.profession = professionname
+class Weapon(models.Model):
 
-    @property
-    def getprofession(self):
-        url = Profession.URL + self.profession
-        req = requests.get(url)
 
-        return_response = {}
-        data = json.loads(req.text)
+class ProfessionSkill(models.Model):
 
-        return_response["name"] = {}
-        return_response["name"] = data["name"]
-        return_response["training"] = {}
-        return_response["training"] = data["training"]
-        return_response["training"] = data["training"]
-        return_response["weapons"] = {}
-        return_response["weapons"] = data["weapons"]
-        '''return_response["weapons"]["axe"] = data["weapons"]["Axe"]
-        return_response["weapons"]["Dagger"] = data["weapons"]["Dagger"]
-        return_response["weapons"]["Mace"] = data["weapons"]["Mace"]
-        return_response["weapons"]["Pistol"] = data["weapons"]["Pistol"]
-        return_response["weapons"]["Sword"] = data["weapons"]["Sword"]
-        return_response["weapons"]["Scepter"] = data["weapons"]["Scepter"]
-        return_response["weapons"]["Focus"] = data["weapons"]["Focus"]
-        return_response["weapons"]["Shield"] = data["weapons"]["Shield"]
-        return_response["weapons"]["Torch"] = data["weapons"]["Torch"]
-        return_response["weapons"]["Warhorn"] = data["weapons"]["Warhorn"]
-        return_response["weapons"]["Greatsword"] = data["weapons"]["Greatsword"]
-        return_response["weapons"]["Hammer"] = data["weapons"]["Hammer"]
-        return_response["weapons"]["Longbow"] = data["weapons"]["Longbow"]
-        return_response["weapons"]["Rifle"] = data["weapons"]["Rifle"]
-        return_response["weapons"]["Shortbow"] = data["weapons"]["Shortbow"]
-        return_response["weapons"]["Staff"] = data["weapons"]["Staff"]
-        return_response["weapons"]["Speargun"] = data["weapons"]["Speargun"]
-        return_response["weapons"]["Spear"] = data["weapons"]["Spear"]
-        return_response["weapons"]["Trident"] = data["weapons"]["Trident"]'''
+class WeaponSkill(models.Model):
 
-        return return_response
+
+class Player(models.Model):
+    id = models.AutoField(primary_key=True)
+    charid = models.ForeignKey(Character)
+    bankid = models.ForeignKey(Bank)
+    gameid = models.ForeignKey(Game)
+    tpid = models.ForeignKey(TradingPost)
+    achievementid = models.ForeignKey(Achievement)
+
+class Achievement(models.Model):
+
+class TradingPost(models.Model):
+
+class Character(models.Model):
+    name = models.TextField(primary_key=True)
+
+
+class Bank(models.Model):
+
+class Game(models.Model):
+
--- gw2_webclient/gw2/views.py
+++ gw2_webclient/gw2/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+import requests
+import json
+
+
+
+class GwClient(object):
+    URL = "https://api.guildwars2.com/v2/"
+    url_services = {
+        "professions": "professions/"
+    }
+
+    def __init__(self, professionname, apikey):
+        super(GwClient, self).__init__()
+        self.profession = professionname
+        self.apikey = apikey
+
+
+    @property
+    def getprofession(self):
+        url = GwClient.URL + GwClient.url_services["professions"] + self.profession
+        req = requests.get(url)
+
+        return_response = {}
+        data = json.loads(req.text)
+
+        return_response["name"] = {}
+        return_response["name"] = data["name"]
+        return_response["training"] = {}
+        return_response["training"] = data["training"]
+        return_response["training"] = data["training"]
+        return_response["weapons"] = {}
+        return_response["weapons"] = data["weapons"]
+
+        return return_response
+
+
+if __name__ == "__main__":
+    api = None
+    client = GwClient("Mesmer", api)
+    print client.getprofession
