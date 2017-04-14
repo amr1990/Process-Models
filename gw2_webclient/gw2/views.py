0b542aceeedb839383985d51deb9454411ae9a76
Antonio Expósito Solis <antonioexposito6@hotmail.com>
2017-04-14 16:20:37 +0200

    Bank implemented

--- gw2_webclient/gw2/views.py
+++ gw2_webclient/gw2/views.py
@@ -23,6 +23,7 @@ url_services = {
     "items": "items/",
     "achievements": "achievements",
     "daily": "/daily",
+    "bank": "bank/"
 }
 
 
@@ -122,6 +123,29 @@ def getInventory(request):
                 return_response_inventory.append((itemname, item["count"]))
 
     return render_to_response('inventory.html',{'inventory': return_response_inventory, 'name':charname},context)
+
+def getBank(request):
+    context = RequestContext(request)
+    charname = request.GET.get('name')
+    if request.user.is_authenticated():
+        user = User.objects.get(id=request.user.id)
+        profile = UserProfile.objects.filter(user=user).get()
+        api = profile.apikey
+
+    url = URL + url_services["account"] + url_services["bank"] + url_services["token"] + api
+    req_bank = requests.get(url)
+    data_bank = json.loads(req_bank.text)
+    return_response_bank = []
+
+    for item in data_bank:
+        if item:
+            url_items = URL + url_services["items"] + str(item["id"])
+            req_items = requests.get(url_items)
+            data_items = json.loads(req_items.text)
+            itemname = data_items["name"]
+            return_response_bank.append((itemname, item["count"]))
+
+    return render_to_response('bank.html',{'bank': return_response_bank, 'name':charname},context)
 
 def getDailyAchievement(request):
     context = RequestContext(request)
