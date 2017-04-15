1d6315b1e0109cdeba710d345e3ea1900a90c56a
Albert Mari <amr1990lol@gmail.com>
2017-04-15 04:23:14 +0200

    From Professions all done, Trading post implemented, refining templates

--- gw2_webclient/gw2/views.py
+++ gw2_webclient/gw2/views.py
@@ -16,7 +16,7 @@ from models import UserProfile
 URL = "https://api.guildwars2.com/v2/"
 url_services = {
     "token": "?access_token=",
-    "professions": "professions/",
+    "professions": "professions",
     "account": "account/",
     "inventory": "/inventory/",
     "character": "characters/",
@@ -26,6 +26,15 @@ url_services = {
     "daily": "/daily",
     "equipment": "/equipment/",
     "bank": "bank/",
+    "spec": "specializations/",
+    "skills": "skills/",
+    "traits": "traits/",
+    "titles": "titles/",
+    "trading_post": "commerce/transactions/",
+    "current": "current/",
+    "history": "history/",
+    "buys": "buys/",
+    "sells": "sells/",
 }
 
 
@@ -54,7 +63,6 @@ def register(request):
             user.set_password(user.password)
             user.save()
 
-
             # Update our variable to tell the template registration was successful.
             registered = True
 
@@ -71,34 +79,14 @@ def register(request):
 
     # Render the template depending on the context.
     return render_to_response(
-            'registration/register.html',
-            {'user_form': user_form, 'registered': registered},
-            context)
-
+        'registration/register.html',
+        {'user_form': user_form, 'registered': registered},
+        context)
 
 
 def homepage(request):
     context = RequestContext(request)
     return render_to_response("homepage.html", context)
-
-
-@property
-def getProfession():
-    url = URL + url_services["professions"] + self.profession
-    req = requests.get(url)
-
-    return_response = {}
-    data = json.loads(req.text)
-
-    return_response["name"] = {}
-    return_response["name"] = data["name"]
-    return_response["training"] = {}
-    return_response["training"] = data["training"]
-    return_response["training"] = data["training"]
-    return_response["weapons"] = {}
-    return_response["weapons"] = data["weapons"]
-
-    return return_response
 
 
 def getInventory(request):
@@ -124,7 +112,7 @@ def getInventory(request):
                 itemname = data_items["name"]
                 return_response_inventory.append((itemname, item["count"]))
 
-    return render_to_response('inventory.html',{'inventory': return_response_inventory, 'name':charname},context)
+    return render_to_response('inventory.html', {'inventory': return_response_inventory, 'name': charname}, context)
 
 
 def getDailyAchievement(request):
@@ -143,7 +131,7 @@ def getDailyAchievement(request):
     for i in dailies:
         for j in i:
             URL_achi = URL + url_services["achievements"] + token + str(j["id"])
-            #print URL_achi
+            # print URL_achi
             req_achi = requests.get(URL_achi)
             data_achi = json.loads(req_achi.text)
             return_response.append((data_achi["name"], data_achi["requirement"]))
@@ -184,17 +172,22 @@ def getCharacterInfo(request):
     data_charinfo = json.loads(req_charinfo.text)
     info_params = data_charinfo.keys()
     char_info = []
-    for item in info_params[2:]:
+    for item in info_params:
         if data_charinfo[item]:
-            res = item + ": " + unicode(data_charinfo[item])
-            char_info.append(res)
+            if item == "title":
+                URL_titles = URL + url_services["titles"] + str(data_charinfo["title"])
+                req_titles = requests.get(URL_titles)
+                data_titles = json.loads(req_titles.text)
+                res = data_titles["name"]
+            else:
+                res = unicode(data_charinfo[item])
+            char_info.append([item, res])
 
     return render_to_response(
         'infochar.html',
-        {'charinfo': char_info,
-         'name': charname
-        },
+        {'charinfo': char_info},
         context)
+
 
 @login_required
 def getGear(request):
@@ -214,18 +207,16 @@ def getGear(request):
 
     for gear in data_gear["equipment"]:
 
-         if gear["id"]:
-                url_items = URL + url_services["items"] + str(gear["id"])
-                req_items = requests.get(url_items)
-                data_items = json.loads(req_items.text)
-                itemname = data_items["name"]
-                itemtype = data_items["type"]
+        if gear["id"]:
+            url_items = URL + url_services["items"] + str(gear["id"])
+            req_items = requests.get(url_items)
+            data_items = json.loads(req_items.text)
+            itemname = data_items["name"]
+            itemtype = data_items["type"]
 
+            return_response_gear.append((itemname, itemtype, data_items["details"]))
 
-                return_response_gear.append((itemname,itemtype,data_items["details"]))
-
-
-         '''for item in gear:
+        '''for item in gear:
             if item:
                 url_items = URL + url_services["items"] + str(item["id"])
                 req_items = requests.get(url_items)
@@ -233,7 +224,7 @@ def getGear(request):
                 itemname = data_items["name"]
                 return_response_inventory.append((itemname, item["count"]))
 '''
-    return render_to_response('gear.html',{'gear':return_response_gear},context)
+    return render_to_response('gear.html', {'gear': return_response_gear}, context)
 
 
 @login_required
@@ -257,10 +248,13 @@ def getBank(request):
             itemname = data_items["name"]
             return_response_bank.append((itemname, item["count"]))
 
-    return render_to_response('bank.html',{'bank': return_response_bank},context)
+    return render_to_response(
+        'bank.html',
+        {'bank': return_response_bank},
+        context)
 
 
-def getWorldBosses(request):
+def getEvents(request):
     context = RequestContext(request)
     url = urllib2.urlopen('https://wiki.guildwars2.com/wiki/World_boss')
     htmlpage = url.read()
@@ -270,15 +264,213 @@ def getWorldBosses(request):
     ignoreFirst = 0
     bs = bs4.BeautifulSoup(htmlpage, "lxml")
     caixa = bs.find("table", {"class": "mech1 mw-collapsible mw-collapsed table"})
-    item = caixa.findAll("tr")
+    items = caixa.findAll("tr")
 
-    for i in item:
+    for item in items:
         if ignoreFirst == 0:
             ignoreFirst += 1
         else:
-            itemClean.append(i.text.encode("utf-8"))
-    for j in itemClean:
-        itemFinal.append([line for line in j.split('\n') if line.strip() != ''])
+            itemClean.append(item.text.encode("utf-8"))
+    for i in itemClean:
+        itemFinal.append([line for line in i.split('\n') if line.strip() != ''])
 
-    return render_to_response('events.html', {'events': itemFinal}, context)
+    return render_to_response(
+        'events.html',
+        {'events': itemFinal},
+        context)
 
+
+def getInfoProfession(request):
+    context = RequestContext(request)
+    URL_professions = URL + url_services["professions"]
+    req_professions = requests.get(URL_professions)
+    data_professions = json.loads(req_professions.text)
+
+    return render_to_response(
+        'professions.html',
+        {'professions': data_professions},
+        context)
+
+
+def getTraining(request, prof_id):
+    context = RequestContext(request)
+    profnameurl = "/" + prof_id
+    URL_professions = URL + url_services["professions"] + profnameurl
+    req_professions = requests.get(URL_professions)
+    data_professions = json.loads(req_professions.text)
+    trainings = data_professions["training"]
+    track_list = []
+    return_list = []
+
+    for training in trainings:
+        for track in training["track"]:
+            if track["type"] == "Skill":
+                url_skills = URL + url_services["skills"] + str(track["skill_id"])
+                req_skills = requests.get(url_skills)
+                data = json.loads(req_skills.text)
+            else:
+                url_traits = URL + url_services["traits"] + str(track["trait_id"])
+                req_traits = requests.get(url_traits)
+                data = json.loads(req_traits.text)
+            track_list.append((data["name"], data["description"], track["type"]))
+        return_list.append([training["name"], track_list])
+        track_list = []
+
+    return render_to_response(
+        'training.html',
+        {'trainings': return_list,
+         'prof': prof_id},
+        context)
+
+
+def getWeapons(request, prof_id):
+    context = RequestContext(request)
+    profnameurl = "/" + prof_id
+    URL_professions = URL + url_services["professions"] + profnameurl
+    req_professions = requests.get(URL_professions)
+    data_professions = json.loads(req_professions.text)
+    weapons = data_professions["weapons"]
+    weaponlist = weapons.keys()
+    data_skills = []
+    response = []
+    skills = []
+    cont = 0
+
+    for key in weaponlist:
+        data_skills.append(weapons[key]["skills"])
+
+    for skill in data_skills:
+        for id in skill:
+            url_wskills = URL + url_services["skills"] + str(id["id"])
+            req_wskills = requests.get(url_wskills)
+            data_wskills = json.loads(req_wskills.text)
+            skills.append((id["slot"], data_wskills["name"], data_wskills["description"]))
+        response.append([weaponlist[cont], skills])
+        skills = []
+        cont += 1
+
+    return render_to_response(
+        'weapons.html',
+        {'weapons': weaponlist,
+         'skills': response,
+         'prof': prof_id},
+        context)
+
+
+def getProfessionSkills(request, prof_id):
+    context = RequestContext(request)
+    profnameurl = "/" + prof_id
+    URL_professions = URL + url_services["professions"] + profnameurl
+    req_professions = requests.get(URL_professions)
+    data_professions = json.loads(req_professions.text)
+    return_skills = []
+
+    for skills in data_professions["skills"]:
+        url_skills = URL + url_services["skills"] + str(skills["id"])
+        req_skills = requests.get(url_skills)
+        data_skills = json.loads(req_skills.text)
+        return_skills.append((data_skills["name"], skills["slot"], data_skills["description"]))
+
+    return render_to_response(
+        'professionskills.html',
+        {'skills': return_skills,
+         'prof': prof_id},
+        context)
+
+
+def tradingPost(request):
+    context = RequestContext(request)
+    return render_to_response("trading_post.html", context)
+
+
+@login_required
+def getTradingPostCurrent(request):
+    context = RequestContext(request)
+
+    if request.user.is_authenticated():
+        user = User.objects.get(id=request.user.id)
+        profile = UserProfile.objects.filter(user=user).get()
+        api = profile.apikey
+    URL_currentinfobuys = URL + url_services["trading_post"] + url_services["current"] \
+                      + url_services["buys"] + url_services["token"] + api
+    URL_currentinfosells = URL + url_services["trading_post"] + url_services["current"] \
+                      + url_services["sells"] + url_services["token"] + api
+    req_currentinfobuys = requests.get(URL_currentinfobuys)
+    req_currentinfosells = requests.get(URL_currentinfosells)
+    data_currentinfobuys = json.loads(req_currentinfobuys.text)
+    data_currentinfosells = json.loads(req_currentinfosells.text)
+
+    return_response_current_buys = []
+    return_response_current_sells = []
+
+    for item in data_currentinfobuys:
+        url_items = URL + url_services["items"] + str(item["item_id"])
+        req_items = requests.get(url_items)
+        data_items = json.loads(req_items.text)
+        itemname = data_items["name"]
+        return_response_current_buys.append((itemname.encode("utf-8"),
+                                        item["quantity"],
+                                        item["price"],
+                                        item["created"].encode("utf-8")))
+
+    for item in data_currentinfosells:
+        url_items = URL + url_services["items"] + str(item["item_id"])
+        req_items = requests.get(url_items)
+        data_items = json.loads(req_items.text)
+        itemname = data_items["name"]
+        return_response_current_sells.append((itemname.encode("utf-8"),
+                                        item["quantity"],
+                                        item["price"],
+                                        item["created"].encode("utf-8")))
+
+    return render_to_response(
+        'trading_post_current.html',
+        {'buys': return_response_current_buys, 'sells': return_response_current_sells },
+        context)
+
+@login_required
+def getTradingPostHistory(request):
+    context = RequestContext(request)
+
+    if request.user.is_authenticated():
+        user = User.objects.get(id=request.user.id)
+        profile = UserProfile.objects.filter(user=user).get()
+        api = profile.apikey
+    URL_historyinfobuys = URL + url_services["trading_post"] + url_services["history"] \
+                      + url_services["buys"] + url_services["token"] + api
+    URL_historyinfosells = URL + url_services["trading_post"] + url_services["history"] \
+                      + url_services["sells"] + url_services["token"] + api
+    req_historyinfobuys = requests.get(URL_historyinfobuys)
+    req_historyinfosells = requests.get(URL_historyinfosells)
+    data_historyinfobuys = json.loads(req_historyinfobuys.text)
+    data_historyinfosells = json.loads(req_historyinfosells.text)
+
+    return_response_history_buys = []
+    return_response_history_sells = []
+
+    for item in data_historyinfobuys:
+        url_items = URL + url_services["items"] + str(item["item_id"])
+        req_items = requests.get(url_items)
+        data_items = json.loads(req_items.text)
+        itemname = data_items["name"]
+        return_response_history_buys.append((itemname.encode("utf-8"),
+                                        item["quantity"],
+                                        item["price"],
+                                        item["created"].encode("utf-8"),
+                                        item["purchased"].encode("utf-8")))
+
+    for item in data_historyinfosells:
+        url_items = URL + url_services["items"] + str(item["item_id"])
+        req_items = requests.get(url_items)
+        data_items = json.loads(req_items.text)
+        itemname = data_items["name"]
+        return_response_history_sells.append((itemname.encode("utf-8"),
+                                        item["quantity"],
+                                        item["price"],
+                                        item["created"].encode("utf-8"),
+                                        item["purchased"].encode("utf-8")))
+
+    return render_to_response(
+        'trading_post_history.html',
+        {'buys': return_response_history_buys, 'sells': return_response_history_sells },
+        context)
--- gw2_webclient/gw2_webclient/urls.py
+++ gw2_webclient/gw2_webclient/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth.views import login, logout
 from gw2 import views
@@ -21,7 +21,14 @@ from gw2 import views
 urlpatterns = [
     url(r'^$', views.homepage, name='homepage'),
     url(r'^dailies/$', views.getDailyAchievement, name='dailies'),
-    url(r'^events/$', views.getWorldBosses, name='events'),
+    url(r'^professions/$', views.getInfoProfession, name='professions'),
+    url(r'^professions/(?P<prof_id>.*)/training/$', views.getTraining, name='training'),
+    url(r'^professions/(?P<prof_id>.*)/skills/$', views.getProfessionSkills, name='professionskills'),
+    url(r'^professions/(?P<prof_id>.*)/weapons/$', views.getWeapons, name='weapons'),
+    url(r'^tradingpost/$', views.tradingPost, name='trading_post'),
+    url(r'^tradingpost/current/$', views.getTradingPostCurrent, name='trading_post_current'),
+    url(r'^tradingpost/history/$', views.getTradingPostHistory, name='trading_post_history'),
+    url(r'^events/$', views.getEvents, name='events'),
     url(r'^bank/$', views.getBank, name='bank'),
     url(r'^characters/$', views.getCharacterList, name='characters'),
     url(r'^characters/info/$', views.getCharacterInfo, name='characterinfo'),
